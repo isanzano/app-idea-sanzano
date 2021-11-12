2bb36150b7877ae09fbffd346889a72758a7a23c
isanzano <isanzano12@gmail.com>
2021-11-11 23:33:42 -0300

    cambios en btn y app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Carrito from './components/Carrito';
-import { Button , Container, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Component } from 'react';
+// import {Container, Row,Col} from 'react-bootstrap' ;
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Carrito />
     </div>
   )
 }
